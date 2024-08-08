e80c3195560ba981be4b2b1a9b65c0ef42fbdef0
FernanJS17 <109115569+FernanJS17@users.noreply.github.com>
2024-08-08 01:54:48 -0600

    feat(paginado en listar):Pantalla Proyectos

--- sigesproc_app/lib/screens/proyectos/proyecto.dart
+++ sigesproc_app/lib/screens/proyectos/proyecto.dart
@@ -11,12 +11,14 @@ class Proyecto extends StatefulWidget {
 }
 
 class _ProyectoState extends State<Proyecto> {
-   int _selectedIndex = 1;
-   Future<List<ProyectoViewModel>>? _proyectosFuture;
+  int _selectedIndex = 1;
+  Future<List<ProyectoViewModel>>? _proyectosFuture;
   TextEditingController _searchController = TextEditingController();
   List<ProyectoViewModel> _proyectosFiltrados = [];
+  int _currentPage = 0;
+  int _rowsPerPage = 10;
 
-   @override
+  @override
   void initState() {
     super.initState();
     _proyectosFuture = ProyectoService.listarProyectos();
@@ -43,61 +45,87 @@ class _ProyectoState extends State<Proyecto> {
       });
     }
   }
-void _onItemTapped(int index) {
+
+  void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  Widget ProyectoRegistro(ProyectoViewModel proyecto, int index) {
-  return ListTile(
-    leading: CircleAvatar(
-      child: Text(
-        (index + 1).toString(),  // Aquí se usa el índice + 1 para mostrar el número secuencial
-        style: TextStyle(color: Colors.black),
-      ),
-      backgroundColor: Color(0xFFFFF0C6),
-    ),
-    title: Text(
-      proyecto.proyNombre ?? 'N/A',
-      style: TextStyle(color: Colors.white),
-    ),
-    subtitle: Text(
-      'Supervisor: ${proyecto.proyDescripcion ?? 'N/A'}',
-      style: TextStyle(color: Colors.white70),
-    ),
-    trailing: Row(
-      mainAxisSize: MainAxisSize.min,
+  void _previousPage() {
+    setState(() {
+      if (_currentPage > 0) {
+        _currentPage--;
+      }
+    });
+  }
+
+  void _nextPage() {
+    setState(() {
+      if (_proyectosFiltrados.length > (_currentPage + 1) * _rowsPerPage) {
+        _currentPage++;
+      }
+    });
+  }
+
+  TableRow _buildProyectoRow(ProyectoViewModel proyecto, int index) {
+    return TableRow(
       children: [
-        Icon(
-          proyecto.proyEstado == true ? Icons.adjust : Icons.adjust,
-          color: proyecto.proyEstado == true ? Colors.red : Colors.green,
+        TableCell(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              (index + 1).toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
-        PopupMenuButton<int>(
-          icon: Icon(Icons.more_vert, color: Colors.white),
-          onSelected: (int result) {
-            if (result == 0) {
-              // Acción 1
-            } else if (result == 1) {
-              // Acción 2
-            }
-          },
-          itemBuilder: (BuildContext context) => <PopupMenuEntry<int>>[
-            const PopupMenuItem<int>(
-              value: 0,
-              child: Text('Acción 1'),
+        TableCell(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              proyecto.proyNombre ?? 'N/A',
+              style: TextStyle(color: Colors.white),
             ),
-            const PopupMenuItem<int>(
-              value: 1,
-              child: Text('Acción 2'),
+          ),
+        ),
+        TableCell(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              proyecto.proyEstado == true ? Icons.adjust : Icons.adjust,
+              color: proyecto.proyEstado == true ? Colors.red : Colors.green,
             ),
-          ],
+          ),
+        ),
+        TableCell(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PopupMenuButton<int>(
+              icon: Icon(Icons.more_vert, color: Colors.white),
+              onSelected: (int result) {
+                if (result == 0) {
+                  // Acción 1
+                } else if (result == 1) {
+                  // Acción 2
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<int>>[
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text('Acción 1'),
+                ),
+                const PopupMenuItem<int>(
+                  value: 1,
+                  child: Text('Acción 2'),
+                ),
+              ],
+            ),
+          ),
         ),
       ],
-    ),
-  );
-}
-
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -110,7 +138,7 @@ void _onItemTapped(int index) {
               'lib/assets/logo-sigesproc.png',
               height: 60,
             ),
-            SizedBox(width: 5), 
+            SizedBox(width: 5),
             Text(
               'SIGESPROC',
               style: TextStyle(
@@ -182,8 +210,7 @@ void _onItemTapped(int index) {
                     ),
                     IconButton(
                       icon: Icon(Icons.filter_list, color: Colors.white54),
-                      onPressed: () {
-                      },
+                      onPressed: () {},
                     ),
                   ],
                 ),
@@ -213,12 +240,106 @@ void _onItemTapped(int index) {
                       ),
                     );
                   } else {
-                    return ListView.builder(
-                      padding: EdgeInsets.only(bottom: 80.0),
-                      itemCount: _proyectosFiltrados.isEmpty ? snapshot.data!.length : _proyectosFiltrados.length,
-                      itemBuilder: (context, index) {
-                        return ProyectoRegistro(_proyectosFiltrados.isEmpty ? snapshot.data![index] : _proyectosFiltrados[index], index);
-                      },
+                    _proyectosFiltrados = _searchController.text.isEmpty
+                        ? snapshot.data!
+                        : _proyectosFiltrados;
+                    final int totalRecords = _proyectosFiltrados.length;
+                    final int startIndex = _currentPage * _rowsPerPage;
+                    final int endIndex = (startIndex + _rowsPerPage > totalRecords)
+                        ? totalRecords
+                        : startIndex + _rowsPerPage;
+
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: Table(
+                              columnWidths: {
+                                0: FlexColumnWidth(1),
+                                1: FlexColumnWidth(4),
+                                2: FlexColumnWidth(2),
+                                3: FlexColumnWidth(2),
+                              },
+                              children: [
+                                TableRow(
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFF171717),
+                                  ),
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'No.',
+                                        style: TextStyle(
+                                          color: Color(0xFFFFF0C6),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Descripción',
+                                        style: TextStyle(
+                                          color: Color(0xFFFFF0C6),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Estado',
+                                        style: TextStyle(
+                                          color: Color(0xFFFFF0C6),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Acciones',
+                                        style: TextStyle(
+                                          color: Color(0xFFFFF0C6),
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                ..._proyectosFiltrados
+                                    .sublist(startIndex, endIndex)
+                                    .asMap()
+                                    .entries
+                                    .map((entry) {
+                                  final index = entry.key;
+                                  final proyecto = entry.value;
+                                  return _buildProyectoRow(proyecto, startIndex + index);
+                                }).toList(),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Mostrando ${startIndex + 1} al ${endIndex} de $totalRecords entradas',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              onPressed: _previousPage,
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.arrow_forward),
+                              onPressed: _nextPage,
+                            ),
+                          ],
+                        ),
+                      ],
                     );
                   }
                 },
