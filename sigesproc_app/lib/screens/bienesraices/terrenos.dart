2430597f6387e8c3c7d086197959e55ab04865c6
FernanJS17 <109115569+FernanJS17@users.noreply.github.com>
2024-09-03 21:13:44 -0600

    fix(mapa de terrenos ahora con google maps)

--- sigesproc_app/lib/screens/bienesraices/terrenos.dart
+++ sigesproc_app/lib/screens/bienesraices/terrenos.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:sigesproc_app/models/bienesraices/terrenoviewmodel.dart';
 import 'package:sigesproc_app/services/apiservice.dart';
 import 'package:sigesproc_app/services/bienesraices/procesoventaservice.dart';
-
-const MAPBOX_TOKEN = ApiService.mapboxTokenFM;
 
 class TerrenosMap extends StatefulWidget {
   @override
@@ -17,6 +14,7 @@ class TerrenosMap extends StatefulWidget {
 class _TerrenosMapState extends State<TerrenosMap> {
   late Future<List<TerrenosViewModel>> _terrenosFuture;
   List<Marker> _markers = [];
+  late GoogleMapController _mapController;
 
   @override
   void initState() {
@@ -52,7 +50,7 @@ class _TerrenosMapState extends State<TerrenosMap> {
   }
 
   Future<void> _addDynamicMarkers(List<TerrenosViewModel> terrenos) async {
-    final colors = ['#FF0000', '#00FF00'];
+    final colors = [BitmapDescriptor.hueRed, BitmapDescriptor.hueGreen];
     final firstFiveTerrenos = terrenos.take(5).toList();
 
     for (var i = 0; i < firstFiveTerrenos.length; i++) {
@@ -61,77 +59,70 @@ class _TerrenosMapState extends State<TerrenosMap> {
       if (coordinates != null) {
         final color = terreno.terrEstado == true ? colors[0] : colors[1];
         _addMarker(coordinates, color, terreno);
-        await Future.delayed(Duration(seconds: 1));
       }
     }
     setState(() {});
   }
 
-  void _addMarker(LatLng coordinates, String color, TerrenosViewModel details) {
+  void _addMarker(LatLng coordinates, double color, TerrenosViewModel details) {
     final marker = Marker(
-      point: coordinates,
-      builder: (context) => GestureDetector(
-        onTap: () {
-          _showDetailsPopup(context, coordinates, details);
-        },
-        child: Icon(
-          Icons.location_on,
-          color: Color(int.parse(color.replaceAll('#', '0xFF'))),
-          size: 40,
-        ),
-      ),
+      markerId: MarkerId(details.terrDescripcion ?? 'Terreno'),
+      position: coordinates,
+      icon: BitmapDescriptor.defaultMarkerWithHue(color),
+      onTap: () {
+        _showDetailsPopup(context, coordinates, details);
+      },
     );
     _markers.add(marker);
   }
 
- void _showDetailsPopup(BuildContext context, LatLng coordinates, TerrenosViewModel details) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        backgroundColor: Colors.black, // Fondo negro
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Descripción: ${details.terrDescripcion ?? 'Desconocida'}',
-              style: TextStyle(
-                color: Color(0xFFFFF0C6),
-                fontWeight: FontWeight.bold, // Títulos en negrita
+  void _showDetailsPopup(BuildContext context, LatLng coordinates, TerrenosViewModel details) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.black, // Fondo negro
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Descripción: ${details.terrDescripcion ?? 'Desconocida'}',
+                style: TextStyle(
+                  color: Color(0xFFFFF0C6),
+                  fontWeight: FontWeight.bold, // Títulos en negrita
+                ),
               ),
-            ),
-            SizedBox(height: 5),
-            Text(
-              'Coordenadas: ${coordinates.latitude.toStringAsFixed(4)}, ${coordinates.longitude.toStringAsFixed(4)}',
-              style: TextStyle(
-                color: Color(0xFFFFF0C6),
-                fontWeight: FontWeight.bold, // Títulos en negrita
+              SizedBox(height: 5),
+              Text(
+                'Coordenadas: ${coordinates.latitude.toStringAsFixed(4)}, ${coordinates.longitude.toStringAsFixed(4)}',
+                style: TextStyle(
+                  color: Color(0xFFFFF0C6),
+                  fontWeight: FontWeight.bold, // Títulos en negrita
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            Align(
-              alignment: Alignment.centerRight, // Alineación a la derecha
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text(
-                  'Cerrar',
-                  style: TextStyle(
-                    color: Color(0xFFFFF0C6),
-                    fontWeight: FontWeight.bold, // Texto del botón en negrita
+              SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerRight, // Alineación a la derecha
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Cerrar',
+                    style: TextStyle(
+                      color: Color(0xFFFFF0C6),
+                      fontWeight: FontWeight.bold, // Texto del botón en negrita
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
-
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -210,24 +201,15 @@ class _TerrenosMapState extends State<TerrenosMap> {
             } else {
               final terrenos = snapshot.data!;
               _addDynamicMarkers(terrenos);
-              return FlutterMap(
-                options: MapOptions(
-                  center: LatLng(15.5, -90.0), // Centra el mapa en América Central
-                  zoom: 4, // Ajusta el zoom para mostrar América Central
+              return GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(15.2, -86.6), // Centra el mapa en América Central
+                  zoom: 7, // zoom centrado en Honduras
                 ),
-                children: [
-                  TileLayer(
-                    urlTemplate:
-                        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-                    additionalOptions: const {
-                      'accessToken': MAPBOX_TOKEN,
-                      'id': 'mapbox/streets-v12'
-                    },
-                  ),
-                  MarkerLayer(
-                    markers: _markers,
-                  ),
-                ],
+                markers: Set<Marker>.of(_markers),
+                onMapCreated: (GoogleMapController controller) {
+                  _mapController = controller;
+                },
               );
             }
           },
