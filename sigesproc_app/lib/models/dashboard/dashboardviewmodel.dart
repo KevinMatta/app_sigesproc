8f7e8a9c91e13c0000651f7c7887bc61d8ca11ca
MFaviola <165686215+MFaviola@users.noreply.github.com>
2024-09-05 13:21:29 -0600

    Fix(Graficos)los graficos de cotizacion

--- sigesproc_app/lib/models/dashboard/dashboardviewmodel.dart
+++ sigesproc_app/lib/models/dashboard/dashboardviewmodel.dart
@@ -2,7 +2,12 @@ class DashboardViewModel {
   String? articulo;
   double? totalCompra;
   String? tipoArticulo;
-
+  int? prov_Id;
+  String? prov_Descripcion;
+  //FLETES
+  //top 5 bodegas
+  String? destino; // Total de la compra (double)
+  int? numeroFletes; // Total de la compra (double)
   // Campos adicionales para las compras mensuales
   int? anhio;
   int? mes;
@@ -14,12 +19,39 @@ class DashboardViewModel {
   String? provDescripcion;
   int? numeroDeCotizaciones;
 
+  //top 5 proyectos
+  int? proy_Id;
+  String? proy_Nombre; // Changed to String
+  double? presupuestoTotal; // Changed to double
+  double? totalFletesDestinoProyecto; // Changed to double
+
+  // Freight Incidences (New)
+  int? incidenciasTotales;
+
+  int? numeroFletesMensuales;
+
+  //
+  int? incidenciasMensuales;
+  int? costoIncidenciaMensuales;
+
+  //top 5 proyectos con mayor presupuesto
   DashboardViewModel({
     this.articulo,
     this.totalCompra,
     this.tipoArticulo,
-    this.anhio,
+    this.prov_Id,
+    this.prov_Descripcion,
+    //FLETES
+    this.destino,
+    this.numeroFletes,
+    this.proy_Id,
+    this.proy_Nombre,
+    this.presupuestoTotal,
+    this.totalFletesDestinoProyecto,
+    this.incidenciasTotales,
     this.mes,
+    this.numeroFletesMensuales,
+    this.anhio,
     this.totalCompraMes,
     this.numeroCompras,
     this.provId,
@@ -30,14 +62,41 @@ class DashboardViewModel {
   // Método para transformar el número del mes en el nombre del mes
   String getNombreMes() {
     List<String> meses = [
-      'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre'
     ];
-    return meses[mes != null ? mes! - 1 : 0]; // Ajuste para que el índice coincida con el número de mes
+    return meses[mes != null
+        ? mes! - 1
+        : 0]; // Ajuste para que el índice coincida con el número de mes
   }
 
   factory DashboardViewModel.fromJson(Map<String, dynamic> json) {
     return DashboardViewModel(
+      prov_Descripcion:
+          json['prov_Descripcion'] ?? '', // Use empty string if null
+
+      prov_Id: json['prov_Id'] ?? 0, // Default to 0 if null
+
+      destino: json['destino'] ?? '', // Use empty string if null
+      numeroFletes: json['numeroFletes'] ?? 0, // Default to 0 if null
+      proy_Id: json['proy_Id'] ?? 0,
+      proy_Nombre: json['proy_Nombre'] ?? '',
+      presupuestoTotal: json['presupuestoTotal']?.toDouble() ?? 0.0,
+      totalFletesDestinoProyecto:
+          json['totalFletesDestinoProyecto']?.toDouble() ?? 0.0,
+      incidenciasTotales: json['incidenciasTotales'] ?? 0,
+
+      numeroFletesMensuales: json['numeroFletesMensuales'] ?? 0,
       articulo: json['articulo'] as String?,
       totalCompra: json['totalCompra'] != null
           ? double.tryParse(json['totalCompra'].toString()) ?? 0.0
@@ -62,8 +121,16 @@ class DashboardViewModel {
       'articulo': articulo,
       'totalCompra': totalCompra,
       'tipoArticulo': tipoArticulo,
-      'anhio': anhio,
+      'destino': destino,
+      'numeroFletes': numeroFletes,
+      'proy_Id': proy_Id,
+      'proy_Nombre': proy_Nombre,
+      'presupuestoTotal': presupuestoTotal,
+      'totalFletesDestinoProyecto': totalFletesDestinoProyecto,
+      'incidenciasTotales': incidenciasTotales,
       'mes': mes,
+      'numeroFletesMensuales': numeroFletesMensuales,
+      'anhio': anhio,
       'totalCompraMes': totalCompraMes,
       'numeroCompras': numeroCompras,
       'prov_Id': provId,
