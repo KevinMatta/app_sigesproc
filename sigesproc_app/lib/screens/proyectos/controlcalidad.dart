010d6ca66de950bdb399fb05bf3f5cd62cee2a5c
FernanJS17 <109115569+FernanJS17@users.noreply.github.com>
2024-09-05 12:58:58 -0600

    fix(correccion de diseño, control calidad)

--- sigesproc_app/lib/screens/proyectos/controlcalidad.dart
+++ sigesproc_app/lib/screens/proyectos/controlcalidad.dart
@@ -232,66 +232,77 @@ Widget build(BuildContext context) {
           ),
         ],
       ),
-      bottom: PreferredSize(
-        preferredSize: Size.fromHeight(90.0),
-        child: Column(
-          children: [
-            Text(
-              'Actividad: $actividad',
-              style: TextStyle(
-                color: Color(0xFFFFF0C6),
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 4.0),
-            Container(
-              height: 2.0,
-              color: Color(0xFFFFF0C6),
-            ),
-            SizedBox(height: 5),
-              Row(
-                children: [
-                  SizedBox(width: 5.0),
-                  GestureDetector(
-                    onTap: () {
-                      // Acción para el botón de "Regresar"
-                      Navigator.pop(context);
-                    },
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.arrow_back,
-                          color: Color(0xFFFFF0C6),
-                        ),
-                        SizedBox(width: 3.0),
-                        Text(
-                          'Regresar',
-                          style: TextStyle(
-                            color: Color(0xFFFFF0C6),
-                            fontSize: 15.0,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 20), // Espacio entre el botón y el texto
-                    Padding(
-                      padding: const EdgeInsets.only(top: 30.0), // Padding de 20 unidades por arriba
-                      child: Text(
-                        'Nuevo Control de Calidad',
-                        style: TextStyle(
-                          color: Color(0xFFFFF0C6),
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-            ],
+     bottom: PreferredSize(
+  preferredSize: Size.fromHeight(90.0),
+  child: Container(
+    decoration: BoxDecoration(
+      border: Border(
+        bottom: BorderSide(
+          color: Colors.black, // Color del borde negro
+          width: 2.0, // Grosor del borde
+        ),
+      ),
+    ),
+    child: Column(
+      children: [
+        Text(
+          'Actividad: $actividad',
+          style: TextStyle(
+            color: Color(0xFFFFF0C6),
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
           ),
         ),
+        SizedBox(height: 4.0),
+        Container(
+          height: 2.0,
+          color: Color(0xFFFFF0C6),
+        ),
+        SizedBox(height: 5),
+        Row(
+          children: [
+            SizedBox(width: 5.0),
+            GestureDetector(
+              onTap: () {
+                // Acción para el botón de "Regresar"
+                Navigator.pop(context);
+              },
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.arrow_back,
+                    color: Color(0xFFFFF0C6),
+                  ),
+                  SizedBox(width: 3.0),
+                  Text(
+                    'Regresar',
+                    style: TextStyle(
+                      color: Color(0xFFFFF0C6),
+                      fontSize: 15.0,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 20), // Espacio entre el botón y el texto
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0), // Padding superior
+              child: Text(
+                'Nuevo Control de Calidad',
+                style: TextStyle(
+                  color: Color(0xFFFFF0C6),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ),
+),
+
 
       iconTheme: const IconThemeData(color: Color(0xFFFFF0C6)),
       actions: <Widget>[
